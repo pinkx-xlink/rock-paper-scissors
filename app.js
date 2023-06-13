030ac5141c4c3564d9b34d983e83fe68bd0116b8
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-06-13 13:55:02 -0400

    Add rock, paper, scissors const

--- app.js
+++ app.js
@@ -5,7 +5,15 @@
         let plaeyerScore = 0;
         let computerScore = 0;
         let moves = 0;
-        
+
+        const playGame = () +. {
+            const rockBtn = document.querySelector('.rock')
+            const paperBtn = document.querySelector('.paper')
+            const scissorsBtn = document.querySelector('.scissors')
+            const playerOptions = [rockBtn,paperBtn,scissorsBtn];
+            const computerOptions = ['rock', 'paper', 'scissors']
+        }
+
     }
 
         //PLAYER SELECT//
