a14637c12638c699821fbc7bff25ce0a985ad8b5
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-06-12 23:16:34 -0400

    Add headers and p div to html

---
+++ app.js
@@ -0,0 +1,23 @@
+ 
+    //JAVASCRIPT//
+    <script>
+
+        //PLAYER SELECT//
+        function getPlayerChoice()
+        
+
+
+        //COMPUTER SELECT//
+        function getComputerChoice()
+
+//PLAY ROUND FUNCTION//
+        function playRound(playerSelection, computerSelection) {
+
+        }
+
+        const playerSelection = "rock";
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+
+
+    </script>
